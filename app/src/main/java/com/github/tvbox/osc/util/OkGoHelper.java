b54612925a57902ad2306b234db0320c4bc570af
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2022-07-24 01:27:27 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
@@ -73,9 +73,6 @@ public class OkGoHelper {
             case 3: {
                 return "https://doh.360.cn/dns-query";
             } 
-            case 4: {
-                return "https://8.8.8.8/dns-query";
-            } 
         }
         return "";
     }
@@ -85,7 +82,6 @@ public class OkGoHelper {
         dnsHttpsList.add("腾讯");
         dnsHttpsList.add("阿里");
         dnsHttpsList.add("360");
-        dnsHttpsList.add("其他");
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor("OkExoPlayer");
         if (Hawk.get(HawkConfig.DEBUG_OPEN, false)) {
